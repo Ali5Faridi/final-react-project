4f0da038f667d7be741690c464782a30afdca4d0
Ali5Faridi <ali.faridi@dci-student.org>
2024-11-04 13:08:04 +0100

    Ali

--- shop-online/src/pages/home/Home.tsx
+++ shop-online/src/pages/home/Home.tsx
@@ -1,20 +1,10 @@
-// import React from 'react'
 
-// function Home() {
-//   return (
-//     <div>
-//       Home
-//     </div>
-//   )
-// }
-
-// export default Home
 
 import React from 'react';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-fuchsia-300">
+    <div className="flex items-center justify-center h-screen bg-fuchsia-400">
       <h1 className="text-4xl font-bold text-gray-800">Welcome to Our Online Store!</h1>
     </div>
   );
