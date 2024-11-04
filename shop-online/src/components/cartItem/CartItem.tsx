b33bb15fda48287678fd260d8b998fb50f67e98a
Ali5Faridi <ali.faridi@dci-student.org>
2024-11-04 09:54:38 +0100

    Ali

--- shop-online/src/components/cartItem/CartItem.tsx
+++ shop-online/src/components/cartItem/CartItem.tsx
@@ -1,18 +1,46 @@
 
 import Button from "../button/Button"
+import { useEffect, useState} from "react";
+import { Link } from "react-router-dom";
+import { getProduct } from "../../services/api";
+import {IProduct} from "../../types/server";
+import { useShoppingCartContext } from "../../context/ShoppingCartContext";
 
-function CartItem() {
+
+interface ICartItem {
+    id: number
+    qty: number
+}
+
+function CartItem({id, qty}: ICartItem) {
+
+    const [product, setProduct] = useState<IProduct>();
+
+
+    const { handleIncreaseProductQty, handleDecreaseProductQty, handleRemoveProduct } = useShoppingCartContext();
+
+  useEffect(() => {
+    getProduct(id).then((data) => {
+      setProduct(data);
+      
+    });
+  }, []);
+
   return (
     <div className="flex mt-4 border-b pb-2">
        
-      <img className="w-56 h-34 rounded p-3" src="./src/assets/iphone-15.jpg" alt="iphone.png" />
+     <Link to={`/product/${id}`}>
+     <img className="w-56 h-34 rounded p-3" src={product?.image} alt="iphone.png" />
+     </Link>
+
+
         <div className="p-4">
-            <h1>Product:Apple</h1>
+            <h1>{product?.title}</h1>
           <div className="mt-3 pt-2">
-          <Button variant="primary">+</Button>
-           <span className="mx-2">2</span>
-           <Button className="" variant="primary">-</Button>
-           <Button className="ml-2" variant="danger">Remove</Button>
+          <Button variant="primary" onClick={() => handleIncreaseProductQty(id)}>+</Button>
+           <span className="mx-2">{qty}</span>
+           <Button className="" variant="primary" onClick={() => handleDecreaseProductQty(id)}>-</Button>
+           <Button className="ml-2" variant="danger" onClick={() => handleRemoveProduct(id)}>Remove</Button>
           </div>
             
         </div>
--- shop-online/src/pages/cart/Cart.tsx
+++ shop-online/src/pages/cart/Cart.tsx
@@ -1,32 +1,32 @@
-import Container from "../../components/container/Container"
-import CartItem from "../../components/cartItem/CartItem"
-import Button from "../../components/button/Button"
-import { useShoppingCartContext } from "../../context/ShoppingCartContext"
+import Container from "../../components/container/Container";
+import CartItem from "../../components/cartItem/CartItem";
+import Button from "../../components/button/Button";
+import { useShoppingCartContext } from "../../context/ShoppingCartContext";
 
 function Cart() {
-
-     const {cartItems} = useShoppingCartContext();
-
-
+  const { cartItems } = useShoppingCartContext();
 
   return (
-    <div className="">
+    <div>
       <Container>
-       <CartItem />
-       <CartItem />
-       <CartItem />
-       <CartItem />   
-       <div className="bg-cyan-100 rounded p-6">
-       <p>Total price: 3256$</p>
-       <p> discount: 15%</p>
-       <p>Final price: 2871$</p>
-      </div>  
-      <Button className="mt-2 ml-10" variant="success">
-        Checkout
-        </Button>  
+        <div className="">
+          {cartItems.map((item) => (
+            <CartItem {...item} />
+          ))}
+          ;
+        </div>
+
+        <div className="bg-cyan-100 rounded p-6">
+          <p>Total price: 3256$</p>
+          <p> discount: 15%</p>
+          <p>Final price: 2871$</p>
+        </div>
+        <Button className="mt-2 ml-10" variant="success">
+          Checkout
+        </Button>
       </Container>
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
