48de7e9b207d1ccd5cb5d0bb5e0f651d4ec9eb93
Ali5Faridi <ali.faridi@dci-student.org>
2024-11-04 13:20:17 +0100

    Ali

--- shop-online/src/pages/home/Home.tsx
+++ shop-online/src/pages/home/Home.tsx
@@ -4,9 +4,17 @@ import React from 'react';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-fuchsia-400">
+    <>
+    <div className="flex items-center justify-center h-screen bg-fuchsia-500">
       <h1 className="text-4xl font-bold text-gray-800">Welcome to Our Online Store!</h1>
+      
     </div>
+    <div>
+       <footer className="bg-gray-800 text-white text-center p-4">
+        <p>&copy; 2024 Our Online Store. All rights reserved.</p>
+      </footer>
+    </div>
+    </>
   );
 };
 
